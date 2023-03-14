56541ecfe22959643e6f9b602daaee5e5fab2ed0
Paul Simroth <paul.simroth@proton.me>
2023-03-14 11:56:09 +0100

    button scrollToTop

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -6,6 +6,11 @@ import { footerVariants } from 'utils/motion';
 import { motion } from 'framer-motion';
 
 function Footer() {
+
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+
   return (
     <motion.footer
       variants={footerVariants}
@@ -16,13 +21,20 @@ function Footer() {
 
       <div className='mx-auto flex flex-col gap-8'>
         <div className="flex items-center justify-between flex-wrap gap-5">
+
           <a href="/#contact">
             <button type="button" className="flex items-center h-fit border-2 border-[#E5E5E5] py-4 px-6 bg-[#18BC9C] rounded-[32px] gap-[12px] hover:scale-110">
               <span className="font-bold text-[12px] md:text-[16px] text-[#F8D1CC]">|CONTACT ME!</span>
             </button>
           </a>
+
+          <button className="flex h-fit border-2 border-[#E5E5E5] p-2 bg-[#18BC9C] rounded-[42px] hover:scale-110" onClick={handleScrollToTop}>
+            <span className="font-bold text-[12px] md:text-[16px] text-[#F8D1CC]"><img src="chevrons-up.svg" alt="Chevrons Up" />Top</span>
+          </button>
+
         </div>
         <div className="flex flex-col">
+
           <div className="mb-[50px] h-[2px] bg-white opacity-10" />
 
           <div className="flex items-center justify-between flex-wrap gap-4 md:gap-8">
@@ -42,7 +54,9 @@ function Footer() {
               <Link href="/imprint" className="text-[12px] lg:text-[20px] text-[#18BC9C] hover:scale-110">Imprint</Link>
               <Link href="/data" className="text-[12px] lg:text-[20px] text-[#18BC9C] hover:scale-110">Data Policy</Link>
             </div>
+            
           </div>
+
         </div>
       </div>
     </motion.footer>
